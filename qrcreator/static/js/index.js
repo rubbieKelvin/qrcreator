d28319fdc246f430dbf290157f9106cd7660d0b6
rubbiekelvin <rubbiekelvinvoltsman@gmail.com>
2020-07-19 13:20:12 +0100

    finished frontend

--- qrcreator/static/js/index.js
+++ qrcreator/static/js/index.js
@@ -15,6 +15,14 @@ window.onload = () => {
             .catch(e => alert(e));
         }
     }
+    
+    document.getElementsByClassName("_navlist_toggler")[0].onclick = (e) => {
+        e.preventDefault();
+        
+        navlist.style.display = listopened ? "none":"initial";
+        listopened = !listopened;
+
+    };
 }
 
 // let is_any = (string, list) => {
@@ -36,3 +44,20 @@ let dataisvalid = (data) => {
     alert(reason);
     return false;
 }
+
+let listopened = false;
+let displayrepaired = true;
+let navlist = document.getElementsByClassName("_nav_links")[0];
+
+window.onresize = () => {
+    if (window.innerWidth > 639 && !displayrepaired){
+        navlist.style.display = "flex";
+        displayrepaired = true;
+        listopened = false;
+    }else if (window.innerWidth < 639){
+        if (displayrepaired){
+                navlist.style.display = "none";
+                displayrepaired = false;
+        }
+    }
+};
