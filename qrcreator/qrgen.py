e8cbc98e311c0bbbedb58719c7ae0483ff55c8f5
rubbiekelvin <rubbiekelvinvoltsman@gmail.com>
2020-07-20 11:33:50 +0100

    finished mobile ui

--- qrcreator/qrgen.py
+++ qrcreator/qrgen.py
@@ -5,7 +5,7 @@ from base64 import b64encode
 def getqr(data):
     img = pyqrcode.create(data)
     buffers = io.BytesIO()
-    img.png(buffers, scale=8)
+    img.png(buffers, scale=10)
     
     encoded = b64encode(buffers.getvalue()).decode("ascii")
     return "data:image/png;base64,"+encoded
